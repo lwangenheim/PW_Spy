9fab5fa2fd8345f748f6dd760e06435f2f131f0d
lwangenheim <root@localhost.localdomain>
2019-05-31 15:40:37 -0400

    potfile magic

--- pw_analysis.py
+++ pw_analysis.py
@@ -5,14 +5,25 @@ import operator
 from collections import Counter
 
 repeat = []
+plaintext_passwords = []
+cracked_hashes = []
+
 
 if len(sys.argv) < 3:
-	print("Usage is: <full hashlist> <plaintext password list>")
+	print("Usage is: <full hashlist> <potfile>")
 	sys.exit()
+def split_pot():
+  print('###################### Splitting Pot File ##################\n')
+  with open(sys.argv[2]) as pot:
+      for password in pot:
+          hash_string, pw_string = password.split(":")
+          cracked_hashes.append(hash_string)
+          plaintext_passwords.append(pw_string) 
+  
 
 #Find how many times a password was reused
 def pw_reuse():
-  print("######################PASSWORD REUSE ######################")
+  print("\n\n\n###################### PASSWORD REUSE ######################\n")
   with open(sys.argv[1]) as full:
       for word in full:
           repeat.append(word.strip())
@@ -25,37 +36,37 @@ def pw_reuse():
 
 #Thanks Joshua Platz for his maskbuilder.py
 def masks():
-  print('\n\n\n######################PASSWORD Masks ######################')
+  print('\n\n\n###################### PASSWORD Masks ######################\n')
   upper=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   lower=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
   digit=["0","1","2","3","4","5","6","7","8","9"]
   masks={}
   words=0
-  with open(sys.argv[2]) as f:
-      for word in f:
-        mask=""
-        for char in word.rstrip('\r\n'):
-          if char in upper:
-            mask=mask+"?u"
-          elif char in lower:
-            mask=mask+"?l"
-          elif char in digit:
-            mask=mask+"?d"
-          else:
-            mask=mask+"?s"
-        if mask not in masks:
-            masks[mask] = 1
-        else:
-            masks[mask] += 1
-            words+=1
+  for word in plaintext_passwords:
+    mask=""
+    for char in word.rstrip('\r\n'):
+      if char in upper:
+        mask=mask+"?u"
+      elif char in lower:
+        mask=mask+"?l"
+      elif char in digit:
+        mask=mask+"?d"
+      else:
+        mask=mask+"?s"
+    if mask not in masks:
+        masks[mask] = 1
+    else:
+        masks[mask] += 1
+        words+=1
 
-      sorteddmask = sorted(masks.items(), key=operator.itemgetter(1), reverse=True)
-      for mask in sorteddmask:
-          if mask[1] >= 10:
-            result = mask[0]+","+str(mask[1])+" Occurances,"+str((float(mask[1])/float(words))*100)+"%"
-            print(result)
+  sorteddmask = sorted(masks.items(), key=operator.itemgetter(1), reverse=True)
+  for mask in sorteddmask:
+      if mask[1] >= 10:
+        result = mask[0]+","+str(mask[1])+" Occurances,"+str((float(mask[1])/float(words))*100)+"%"
+        print(result)
 
 
 #Call the functions
+split_pot()
 pw_reuse()
 masks()
